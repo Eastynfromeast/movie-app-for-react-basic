2e0f643a26371a59624870a2342d0b9371d7280e
Eastyn <eastynwork@gmail.com>
2024-06-19 22:49:10 +0900

    set react-router-dom and add a router

--- src/App.js
+++ src/App.js
@@ -1,33 +1,19 @@
-import { useEffect, useState } from "react";
-import Movie from "./Movie";
-function App() {
-	const [isLoading, setIsLoading] = useState(true);
-	const [movies, setMovies] = useState([]);
-	const getMovies = async () => {
-		const json = await (await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year")).json();
-		setMovies(json.data.movies);
-		setIsLoading(false);
-	};
-	useEffect(() => {
-		getMovies();
-	}, []);
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
-	return (
-		<div>
-			{isLoading ? (
-				<h1>Loading...</h1>
-			) : (
-				<>
-					<h1>Welcome to My Movie List!</h1>
-					<ul>
-						{movies.map(movie => (
-							<Movie key={movie.id} title={movie.title} coverImg={movie.medium_cover_image} summary={movie.summary} genres={movie.genres} />
-						))}
-					</ul>
-				</>
-			)}
-		</div>
-	);
+function App() {
+	const router = createBrowserRouter([
+		{
+			path: "/",
+			element: <Home />,
+		},
+		{
+			path: "/movie/:id",
+			element: <Detail />,
+		},
+	]);
+	return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/routes/Home.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import Movie from "../components/Movie";
+
+function Home() {
+	const [isLoading, setIsLoading] = useState(true);
+	const [movies, setMovies] = useState([]);
+	const getMovies = async () => {
+		const json = await (await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year")).json();
+		setMovies(json.data.movies);
+		setIsLoading(false);
+	};
+	useEffect(() => {
+		getMovies();
+	}, []);
+
+	return (
+		<div>
+			{isLoading ? (
+				<h1>Loading...</h1>
+			) : (
+				<>
+					<h1>Welcome to My Movie List!</h1>
+					<ul>
+						{movies.map(movie => (
+							<Movie key={movie.id} title={movie.title} coverImg={movie.medium_cover_image} summary={movie.summary} genres={movie.genres} />
+						))}
+					</ul>
+				</>
+			)}
+		</div>
+	);
+}
+
+export default Home;
