5ad65c2c840a53d7425a6f25bc07770f49004783
Eastyn <eastynwork@gmail.com>
2024-06-19 23:52:31 +0900

    deploy the repository successfully after adding basename process.env.PUBLIC_URL

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 
 function App() {
-	const router = createBrowserRouter([
+	const router = createBrowserRouter(
+		[
+			{
+				path: "/",
+				element: <Home />,
+			},
+			{
+				path: "/movie/:id",
+				element: <Detail />,
+			},
+		],
 		{
-			path: "/",
-			element: <Home />,
-		},
-		{
-			path: "/movie/:id",
-			element: <Detail />,
-		},
-	]);
+			basename: `${process.env.PUBLIC_URL}`,
+		}
+	);
 	return <RouterProvider router={router} />;
 }
 
